c0714e02a8c5e833af1a240beb8d056488473a36
ksllive <zhenya-kiselev-97@mail.ru>
2020-07-22 08:36:56 +0300

    Настройка сцены.

--- Snake/GameViewController.swift
+++ Snake/GameViewController.swift
@@ -15,23 +15,19 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
+        setupScene(for: view)
     }
 
+    func setupScene(for view: UIView) {
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
